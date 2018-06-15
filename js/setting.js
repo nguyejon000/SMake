bc88ff41c9db1f40f31239b09c83795922997fde
nguyejon000 <37934299+nguyejon000@users.noreply.github.com>
2018-06-15 15:38:58 -0700

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -1,8 +1,8 @@
 var mycanvas = document.getElementById('mycanvas');
 var ctx = mycanvas.getContext('2d');
 var snakeSize = 20; 
-var w = 700;
-var h = 700;
+var w = 500;
+var h = 500;
 var score = 0;
 var snake;
 var snakeSize = 20;
